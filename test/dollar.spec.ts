c18cdeae8612aff0add4bc95f787dc5a9fd6e7e0
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-10 21:53:48 +0900

    test: change the test to remove the side effect

--- test/dollar.spec.ts
+++ test/dollar.spec.ts
@@ -2,8 +2,8 @@ import { Dollar } from '../src/dollar'
 
 it('$5 x 2 = $10', () => {
   const five = new Dollar(5)
-  five.times(2)
-  expect(five.amount).toBe(10)
-  five.times(3)
-  expect(five.amount).toBe(15)
+  const product1 = five.times(2)
+  expect(product1.amount).toBe(10)
+  const product2 = five.times(3)
+  expect(product2.amount).toBe(15)
 })
